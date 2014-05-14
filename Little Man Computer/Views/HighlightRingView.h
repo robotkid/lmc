5cfb697f5af9b5bd3b3fe6627441067de19b9230
Dan Horwood <gh@dpj.sent.com>
2014-05-14 09:05:55 +0800

    started animation code

---
+++ Little Man Computer/Views/HighlightRingView.h
@@ -0,0 +1,13 @@
+//
+//  highlightRingView.h
+//  Little Man Computer
+//
+//  Created by Dan Horwood on 14/05/2014.
+//  Copyright (c) 2014 Dan Horwood. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface highlightRingView : NSView
+
+@end
---
+++ Little Man Computer/Views/HighlightRingView.m
@@ -0,0 +1,29 @@
+//
+//  highlightRingView.m
+//  Little Man Computer
+//
+//  Created by Dan Horwood on 14/05/2014.
+//  Copyright (c) 2014 Dan Horwood. All rights reserved.
+//
+
+#import "highlightRingView.h"
+
+@implementation highlightRingView
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+    [super drawRect:dirtyRect];
+    
+    // Drawing code here.
+}
+
+@end
